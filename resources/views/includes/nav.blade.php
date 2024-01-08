b0fc0410651b65ba86711be0ce365712a98141ee
EverlastingLilflo <anjorin199@gmail.com>
2024-01-09 00:48:53 +0100

    changes

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -1,18 +1,19 @@
-<div class=" h-[76px] flex justify-around font-lexend ">
+<div class=" h-[76px] flex justify-around items-center font-lexend ">
     {{-- log --}}
-    <div class="flex">
-        <img src="{{asset('svg/menu.svg')}}" class=" w-6 h-6 block md:hidden" alt="">
-        <img src="{{ asset('svg/Logo.svg') }}" alt="">
+    <div class="flex items-center gap-4">
+        <img src="{{asset('svg/menu.svg')}}" class=" w-6 h-6 block md:hidden" alt="menuSvg">
+        <img src="{{ asset('svg/Logo.svg') }}" alt="logo">
     </div>
 
     {{-- search --}}
-    <div>
+    <div class="hidden md:block">
         <input type="search" value="" placeholder="Search"
-            class="hidden md:block text-base font-normal text-[#C8C9CB] border-[#F4F4F4] md:pl-2 md:px-40 md:py-2 lg:pl-5 lg:px-56 lg:py-4 border rounded-3xl">
+            class=" text-base font-normal text-[#C8C9CB] border-[#F4F4F4] md:pl-2 md:px-40 md:py-2 lg:pl-5 lg:px-56 lg:py-4 border rounded-3xl">
     </div>
 
     {{-- your account and cartlist --}}
     <div>
-        <img src="{{ asset('svg/Logo.svg') }}" alt="">
+        <p>Your Account</p>
+        <img src="{{asset('')}}" alt="cartIcon">
     </div>
 </div>
