f3e6a930efc1683520bf346025b0b23fe025399a
EverlastingLilflo <anjorin199@gmail.com>
2024-01-09 00:44:05 +0100

    writing the screen

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -1,19 +1,18 @@
-<div class=" h-[76px] flex font-lexend ">
+<div class=" h-[76px] flex justify-around font-lexend ">
     {{-- log --}}
-    <div>
+    <div class="flex">
+        <img src="{{asset('svg/menu.svg')}}" class=" w-6 h-6 block md:hidden" alt="">
         <img src="{{ asset('svg/Logo.svg') }}" alt="">
     </div>
 
-    <div class="flex items-center justify-center ">
-        {{-- search --}}
-        <div>
-            <input type="search" value="" placeholder="Search"
-                class="text-base font-normal text-[#C8C9CB] border-[#F4F4F4] pl-5 px-56 py-4 border rounded-3xl">
-        </div>
+    {{-- search --}}
+    <div>
+        <input type="search" value="" placeholder="Search"
+            class="hidden md:block text-base font-normal text-[#C8C9CB] border-[#F4F4F4] md:pl-2 md:px-40 md:py-2 lg:pl-5 lg:px-56 lg:py-4 border rounded-3xl">
+    </div>
 
-        {{-- your account and cartlist --}}
-        <div>
-            <img src="{{ asset('svg/Logo.svg') }}" alt="">
-        </div>
+    {{-- your account and cartlist --}}
+    <div>
+        <img src="{{ asset('svg/Logo.svg') }}" alt="">
     </div>
 </div>
