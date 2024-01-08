cc3df2745cf165ba8929d79f82fd0ce55b2d836a
EverlastingLilflo <anjorin199@gmail.com>
2024-01-09 00:20:16 +0100

    second nav

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -1,5 +1,19 @@
+<div class=" h-[76px] flex font-lexend ">
+    {{-- log --}}
+    <div>
+        <img src="{{ asset('svg/Logo.svg') }}" alt="">
+    </div>
 
+    <div class="flex items-center justify-center ">
+        {{-- search --}}
+        <div>
+            <input type="search" value="" placeholder="Search"
+                class="text-base font-normal text-[#C8C9CB] border-[#F4F4F4] pl-5 px-56 py-4 border rounded-3xl">
+        </div>
 
-<div class=" h-[76px]">
-    <img src="{{asset('svg/Logo.svg')}}" alt="">
+        {{-- your account and cartlist --}}
+        <div>
+            <img src="{{ asset('svg/Logo.svg') }}" alt="">
+        </div>
+    </div>
 </div>
