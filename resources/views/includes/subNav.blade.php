d596198bd83cab0d65e98914781b47e289ddee8c
EverlastingLilflo <anjorin199@gmail.com>
2024-01-08 20:48:44 +0100

    get the current time in laravel carbon

--- resources/views/includes/subNav.blade.php
+++ resources/views/includes/subNav.blade.php
@@ -1,6 +1,9 @@
 
 
 
-<div class=" bg-[#05422C] h-9 flex justify-center items-center">
-    <p class=" text-white font-lexend text-[10px] md:text-base ">LIMITED OFFER: 30% OFF. Use RABBIT30 at Checkout.</p>
+<div class=" bg-[#05422C] text-white font-lexend h-9 flex justify-center items-center">
+    <p class="  text-[10px] md:text-base ">LIMITED OFFER: 30% OFF. Use RABBIT30 at Checkout.</p>
+    <p>{{ \Carbon\Carbon::now()->format('H:i:s') }}</p>
+
 </div>
+
