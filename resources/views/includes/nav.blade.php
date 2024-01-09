1d5bee6092c89863e7c1fcf58463a02ead7dbcf0
EverlastingLilflo <anjorin199@gmail.com>
2024-01-09 17:43:59 +0100

    changes

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -1,11 +1,14 @@
+
+
 <div class=" h-[76px] flex justify-around items-center font-lexend ">
     {{-- log --}}
     <div class="flex items-center gap-2 sm:gap-4">
-        <ul class="menu  text-white list-none pt-16 fixed -translate-y-[100%] transition-transform duration-700 top-0 left-0 right-0 bottom-0 z-[99] bg-black">
+        <ul
+            class="menu  text-white list-none pt-16 fixed -translate-y-[100%] transition-transform duration-700 top-0 left-0 right-0 bottom-0 z-[99] bg-black">
             <li>
                 Shop All
             </li>
-    
+
             <li>
                 <div class="group relative ">
                     <!-- Trigger button -->
@@ -13,13 +16,14 @@
                         <span>Flower</span>
                         <img src="{{ asset('svg/arrow-down.svg') }}" alt="arrow down icon">
                     </button>
-    
+
                     <!-- Dropdown content -->
                     <ul class="hidden absolute group-hover:block w-32">
                         <li><a href="#"
                                 class="rounded-t bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap">Option
                                 1</a></li>
-                        <li><a href="#" class="bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap">Option
+                        <li><a href="#"
+                                class="bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap">Option
                                 2</a></li>
                         <li><a href="#"
                                 class="rounded-b bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap">Option
@@ -37,14 +41,15 @@
                         <span>Concentrates</span>
                         <img src="{{ asset('svg/arrow-down.svg') }}" alt="arrow down icon">
                     </button>
-    
+
                     <!-- Dropdown content -->
                     <ul class="hidden absolute group-hover:block">
                         <li><a href="#"
                                 class="rounded-t bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap">Option
                                 1</a></li>
                         <li><a href="#"
-                                class="bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap">Option 2</a></li>
+                                class="bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap">Option 2</a>
+                        </li>
                         <li><a href="#"
                                 class="rounded-b bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap">Option
                                 3</a></li>
@@ -61,14 +66,15 @@
                         <span>Promotions/Bundles</span>
                         <img src="{{ asset('svg/arrow-down.svg') }}" alt="arrow down icon">
                     </button>
-    
+
                     <!-- Dropdown content -->
                     <ul class="hidden absolute group-hover:block">
                         <li><a href="#"
                                 class="rounded-t bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap">Option
                                 1</a></li>
                         <li><a href="#"
-                                class="bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap">Option 2</a></li>
+                                class="bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap">Option 2</a>
+                        </li>
                         <li><a href="#"
                                 class="rounded-b bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap">Option
                                 3</a></li>
@@ -82,14 +88,15 @@
                         <span>Support</span>
                         <img src="{{ asset('svg/arrow-down.svg') }}" alt="arrow down icon">
                     </button>
-    
+
                     <!-- Dropdown content -->
                     <ul class="hidden absolute group-hover:block w-32">
                         <li><a href="#"
                                 class="rounded-t bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap">Option
                                 1</a></li>
                         <li><a href="#"
-                                class="bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap">Option 2</a></li>
+                                class="bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap">Option 2</a>
+                        </li>
                         <li><a href="#"
                                 class="rounded-b bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap">Option
                                 3</a></li>
@@ -102,13 +109,14 @@
             <li>
                 Blog
             </li>
-          </ul>
-          <button class="hamburger block md:hidden">
+        </ul>
+        <button class="hamburger block md:hidden">
 
-            <i class="closeIcon material-icons hidden  absolute z-[100] top-0 left-0 p-6 text-white bi bi-x-square text-[2rem]"></i>
-        <img src="{{asset('svg/menu.svg')}}" class="menuIcon w-6 h-6 cursor-pointer" alt="menuSvg">
-    </button>
-    <img src="{{ asset('svg/Logo.svg') }}" class=" w-28 h-28 sm:w-36 sm:h-36 md:h-44 md:w-44" alt="logo">
+            <i
+                class="closeIcon material-icons hidden  absolute z-[100] top-0 left-0 p-6 text-white bi bi-x-square text-[2rem]"></i>
+            <img src="{{ asset('svg/menu.svg') }}" class="menuIcon w-6 h-6 cursor-pointer" alt="menuSvg">
+        </button>
+        <img src="{{ asset('svg/Logo.svg') }}" class=" w-28 h-28 sm:w-36 sm:h-36 md:h-44 md:w-44" alt="logo">
 
     </div>
 
@@ -116,10 +124,10 @@
     <div class="hidden md:flex gap-4 items-center">
         <input type="search" value="" placeholder="Search"
             class=" text-base font-normal text-[#C8C9CB] border-[#F4F4F4] md:pl-2 md:px-40 md:py-2 lg:pl-5 lg:px-56 lg:py-4 border rounded-3xl">
-            <button>
-            <img src="{{asset('svg/Button.svg')}}" alt="search icon">
+        <button>
+            <img src="{{ asset('svg/Button.svg') }}" alt="search icon">
 
-            </button>
+        </button>
     </div>
 
     {{-- your account and cartlist --}}
@@ -127,11 +135,10 @@
         <p class=" text-[10px] md:text-base">Your Account</p>
         <p class=" text-base">|</p>
         <div>
-        <img src="{{asset('svg/bag-2.png')}}" alt="cartIcon">
-        <div class="bg-[#EB2606] w-6 h-6 rounded-full text-white absolute my-[-14px] mx-3">
-            <center>0</center>
+            <img src="{{ asset('svg/bag-2.png') }}" alt="cartIcon">
+            <div class="bg-[#EB2606] w-6 h-6 rounded-full text-white absolute my-[-14px] mx-3">
+                <center>0</center>
+            </div>
         </div>
     </div>
-    </div>
 </div>
-
