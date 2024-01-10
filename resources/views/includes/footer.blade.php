f9c703befbbb7561bcf8ae81747d7f9dbad4d667
EverlastingLilflo <anjorin199@gmail.com>
2024-01-10 10:17:21 +0100

    added the coupon background

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -2,18 +2,18 @@
 
 <div class=" h-[107.9rem] sm:h-[80.125rem] bg-gradient-to-b from-[#1A1E26] to-[#01100B] overflow-hidden">
     <div class="flex justify-center absolute top-[70rem] inset-0">
-        <div class="w-[90%] bg-[#05422C] h-[28.7rem] rounded-xl font-lexend  text-white absolute">
-            <h1 class="font-semibold text-[4rem] w-[80%] px-20 pt-10">
+        <div class="w-[85%] bg-[#05422C] h-[30rem] rounded-xl font-lexend  text-white absolute">
+            <h1 class="font-semibold text-[3.5rem] w-[75%] px-20 pt-10">
                 UNLOCK 20% OFF YOUR FIRST ORDER
             </h1>
-            <div class="px-20">
-                <p class="pt-5 pb-10">
+            <div class="px-20 ">
+                <p class="pt-5 pb-10 text-xl text-[#C3D2CC]">
                     Reveal coupon code by entering your email
                 </p>
-                <hr class="w-[90%]">
-                <div>
-                    <input type="email" name="email" id="email">
-                    <button class="bg-[#17AF26] py-[1.5rem] px-[3rem] rounded-[3rem] text-base">Reveal Coupon</button>
+                <hr class="w-[99%] border-[#346654]">
+                <div class="translate-y-8 ">
+                    <input type="email" name="email" id="email" class=" py-6 px-10 w-[75%] rounded-[3rem] bg-transparent border border-[#346654] text-base placeholder:text-base" placeholder="Email Address">
+                    <button class="bg-[#17AF26] py-[1.5rem] px-[3rem] rounded-[3rem] text-base mx-6">Reveal Coupon</button>
                 </div>
             </div>
         </div>
