8374deb38ffb017ed92a96509c49ee44b070b63d
EverlastingLilflo <anjorin199@gmail.com>
2024-01-15 23:01:08 +0100

    added new background image for my landing page

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -2,8 +2,9 @@
 
 
 @section('content')
-<div class=" h-[71rem]">
-   
-</div>
-    
+<div class="md:h-[45rem] h-[44rem] relative">
+    <img src="{{asset('svg/MaskGroup.svg')}}" alt="background" class="absolute inset-0 w-full h-full object-cover">
+    <img src="{{asset('img/Background.png')}}" alt="backgroundOval" class="absolute w-full h-full object-cover">
+ </div>
+ 
 @endsection
