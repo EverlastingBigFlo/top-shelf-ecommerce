962dd28e786f9f86a920c827dd4c5c03ae4e7719
EverlastingLilflo <anjorin199@gmail.com>
2024-01-18 22:22:59 +0100

    added bg image

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -3,8 +3,9 @@
 
 @section('content')
 <div class="md:h-[45rem] h-[44rem] relative">
-    <img src="{{asset('svg/MaskGroup.svg')}}" alt="background" class="absolute inset-0 w-full h-full object-cover">
-    <img src="{{asset('img/Background.png')}}" alt="backgroundOval" class="absolute w-full h-full object-cover">
- </div>
+    <div class="absolute inset-0 w-full h-full bg-cover md:bg-contain" style="background-image: url('{{asset('svg/MaskGroup.svg')}}')"></div>
+    <div class="absolute inset-0 w-full h-full bg-cover md:bg-contain" style="background-image: url('{{asset('img/Background.png')}}')"></div>
+</div>
+
  
 @endsection
