8adf8d91a36a5b46a197a2b80502a34a8925fc88
USER <owner@USERs-MacBook-Air.local>
2024-02-15 10:51:46 +0100

    changes

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -3,8 +3,8 @@
 
 @section('content')
 <div class="md:h-[45rem] h-[44rem] relative">
-    <div class="absolute inset-0 w-full h-full bg-cover md:bg-contain" style="background-image: url('{{asset('svg/MaskGroup.svg')}}')"></div>
-    <div class="absolute inset-0 w-full h-full bg-cover md:bg-contain" style="background-image: url('{{asset('img/Background.png')}}')"></div>
+    <div ><img src="{{asset('svg/MaskGroup.svg')}}" alt="background image" class="absolute inset-0 xxs:w-screen xxs:h-screen md:w-full bg-cover "></div>
+    <div><img src="{{asset('img/Background.png')}}" alt="background image" class="absolute inset-0 xxs:w-screen md:w-full xxs:h-screen bg-cover"></div>
 </div>
 
  
